f3d91a7f7fb5c79ad8fb3ea3d1b90170c379e6f9
16viu09m <16viu09m@gmail.com>
2018-01-14 15:07:47 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build Status](https://travis-ci.org/16viu09m/lab06.svg?branch=master)](https://travis-ci.org/16viu09m/lab06)
+[![Build Status](https://travis-ci.org/16viu09m/lab07.svg?branch=master)](https://travis-ci.org/16viu09m/lab07)
--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout);  //документируем функции print
+void print(const std::string& text, std::ofstream& out);  //документируем функции print
